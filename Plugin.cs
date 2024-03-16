2e6ca413594f584c1a3d2d686caba4a49bd79aaf
Ccode-lang <coop112097@gmail.com>
2024-03-15 17:40:55 -0700

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,33 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using LethalLib.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static LethalLib.Modules.Enemies;
+
+namespace SteamHaunt
+{
+    [BepInPlugin(Plugin.MyGuid, Plugin.PluginName, Plugin.VersionString)]
+    [BepInDependency(LethalLib.Plugin.ModGUID)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private const string MyGuid = "Ccode.SteamHaunt";
+        private const string PluginName = "SteamHaunt";
+        private const string VersionString = "0.0.1";
+
+        private static readonly Harmony Harmony = new Harmony(MyGuid);
+
+        public static ManualLogSource Log;
+
+        public void Awake()
+        {
+            Harmony.PatchAll();
+            Logger.LogInfo(PluginName + " " + VersionString + " " + "loaded.");
+            Log = Logger;
+        }
+    }
+}
